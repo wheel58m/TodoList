4f3b2bbaa9e9d3020564127dce850a7f66b08eb9
Austin Wheeler <wheel58m@gmail.com>
2023-05-02 06:04:53 +0000

    Added Menu

--- Program.cs
+++ Program.cs
@@ -12,9 +12,61 @@
 
 // MAIN -----------------------------------------------------------------------/
 // Display Todo List & Menu ---------------------------------------------------/
-Console.Clear();
-Console.WriteLine("Here is your Todo List:");
-DisplayTodoList();
+bool error = false;
+string message = null;
+
+while (true) {
+    Console.Clear();
+
+    // Display Todo List
+    Console.WriteLine("Here is your Todo List:");
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    DisplayTodoList();
+    Console.ResetColor();
+
+    // Display Any Messages
+    if (error) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+        error = false;
+        message = null;
+    } else {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ResetColor();
+        message = null;
+    }
+
+    // Display Instructions
+    Console.WriteLine("What would you like to do? (1) Add Todo, (2) Remove Todo, (3) Edit Todo, (4) Exit");
+    Console.Write("Enter a Number: ");
+    ConsoleKeyInfo key = Console.ReadKey();
+
+    switch (key.KeyChar) {
+        // Add Todo
+        case '1':
+            // AddTodo();
+            break;
+        // Remove Todo
+        case '2':
+            // RemoveTodo();
+            break;
+        // Edit Todo
+        case '3':
+            // EditTodo();
+            break;
+        // Exit
+        case '4':
+            Console.Clear();
+            return;
+        default:
+            error = true;
+            message = "Invalid Input. Please Try Again.";
+            break;
+    }
+}
+
 
 // METHOD: Display Todo List --------------------------------------------------/
 void DisplayTodoList() {
@@ -30,4 +82,5 @@ void DisplayTodoList() {
             Console.WriteLine($"{i + 1}. {todoList[i]}");
         }
     }
+    Console.WriteLine();
 }
